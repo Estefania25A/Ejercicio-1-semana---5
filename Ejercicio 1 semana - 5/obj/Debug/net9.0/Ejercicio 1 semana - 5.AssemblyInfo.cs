8a6d9a53d585a34cee37f1e76b4f2867aa4502ef
ESTEFANIA DAYANARA ANCHUNDIA TOAZA <ed.anchundiat@uea.edu.ec>
2025-01-10 22:08:34 -0500

    subir

--- Ejercicio 1 semana - 5/obj/Debug/net9.0/Ejercicio 1 semana - 5.AssemblyInfo.cs
+++ Ejercicio 1 semana - 5/obj/Debug/net9.0/Ejercicio 1 semana - 5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 1 semana - 5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34da733d2004decf487d02f914d360259f3d4641")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 1 semana - 5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 1 semana - 5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Ejercicio 3/obj/Debug/net9.0/Ejercicio 3.AssemblyInfo.cs
+++ Ejercicio 3/obj/Debug/net9.0/Ejercicio 3.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio 3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34da733d2004decf487d02f914d360259f3d4641")]
-[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 2")]
+[assembly: System.Reflection.AssemblyProductAttribute("Ejercicio 3")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio 3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
